58033143cda9f661c3d86794d685e462c5d8a0a0
Kanduri Abhinay <kanduriabhinay@gmail.com>
2023-01-26 12:01:55 +0530

    changes

--- finkraft/src/GridData.js
+++ finkraft/src/GridData.js
@@ -1,24 +1,73 @@
 import React, { useEffect, useState } from "react";
 import { US_DATA, VEHICLES_DATA } from "./constants";
+import Table from "react-bootstrap/Table";
+// import Pagination from "react-bootstrap/Pagination";
+
+// import Button from "react-bootstrap/Button";
 const GridData = (props) => {
+  // const [totalData, updateTotalData] = useState(null);
   const [data, updateData] = useState(null);
+  const [sortData, updateSortData] = useState({});
+  // const [activePage, updateActivePage] = useState(1);
   useEffect(() => {
     let url = "";
-    if (props.selectedData == "us-data") {
+    if (props.selectedData === "us-data") {
       url = US_DATA;
-    } else if (props.selectedData == "vehicle-data") {
+    } else if (props.selectedData === "vehicle-data") {
       url = VEHICLES_DATA;
     }
     fetch(url)
       .then((resp) => resp.json())
       .then((res) => {
+        if (props.selectedData === "us-data") res = res.data;
+        // res.forEach((element, ind) => {
+        //   element["id"] = ind;
+        // });
         updateData(res);
       });
-  }, [props.selectedData]);
-
+  }, []);
+  useEffect(() => {
+    if (sortData.type && sortData.column && data.length > 0) {
+      let sortedData = data.sort((a, b) => {
+        if (sortData.type === "asc")
+          return a[sortData.column] < b[sortData.column] ? -1 : 1;
+        else if (sortData.type === "desc")
+          return b[sortData.column] < a[sortData.column] ? -1 : 1;
+      });
+      updateData(sortedData);
+    }
+  }, [sortData.type, sortData.column]);
+  function handleButton(col, typ) {
+    updateSortData({ column: col, type: typ });
+  }
   if (!data) return <div></div>;
-
-  return <div></div>;
+  if (data.length === 0) return <h1>no data found</h1>;
+  return (
+    <div>
+      <Table striped bordered hover size="sm">
+        <thead>
+          <tr>
+            {Object.keys(data[0]).map((item) => (
+              <th>
+                {item}
+                <button onClick={() => handleButton(item, "asc")}>asc</button>
+                <button onClick={() => handleButton(item, "desc")}>desc</button>
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item) => (
+            <tr key={item["id"]}>
+              {Object.values(item).map((d) => (
+                <td>{d}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
+  );
 };
 
 export default GridData;
--- finkraft/src/Home.js
+++ finkraft/src/Home.js
@@ -4,25 +4,25 @@ import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 const Home = (props) => {
   function submitHandler(e) {
-    e.preventDefault();
-    props.updateSelectedData(e.target[0].value);
+    // e.preventDefault();
+    props.updateSelectedData(document.getElementById("dropdown").value);
     // console.log(e.target[0].value);
     // window.location.href = window.location.href + "grid";
   }
   return (
-    <Form onSubmit={submitHandler}>
+    <Form>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Text className="text-muted">
           Kindly select the type of data you want from below dropdown
         </Form.Text>
       </Form.Group>
-      <Form.Select aria-label="Default select example">
+      <Form.Select aria-label="Default select example" id="dropdown">
         <option>Open this select menu</option>
         <option value="vehicle-data">Vehicle Data</option>
         <option value="us-data">US Data</option>
       </Form.Select>
       <Link to="/grid">
-        <Button variant="primary" type="submit">
+        <Button variant="primary" type="submit" onClick={submitHandler}>
           Submit
         </Button>
       </Link>
